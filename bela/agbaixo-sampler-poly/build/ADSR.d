c6594d530fc6d57efc1ed5df8197caf7b554973e
João Tragtenberg <tragtenberg@gmail.com>
2025-05-21 20:01:21 -0300

    delete build files

--- bela/agbaixo-sampler-poly/build/ADSR.d
+++
@@ -1,8 +0,0 @@
-/root/Bela/projects/agbaixo-sampler-poly/build/ADSR.o: \
-  /root/Bela/projects/agbaixo-sampler-poly/ADSR.cpp \
-  /root/Bela/projects/agbaixo-sampler-poly/ADSR.h \
-  /root/Bela/projects/agbaixo-sampler-poly/Ramp.h
-
-/root/Bela/projects/agbaixo-sampler-poly/ADSR.h:
-
-/root/Bela/projects/agbaixo-sampler-poly/Ramp.h:
--- bela/agbaixo-sampler-poly/build/Ramp.d
+++
@@ -1,5 +0,0 @@
-/root/Bela/projects/agbaixo-sampler-poly/build/Ramp.o: \
-  /root/Bela/projects/agbaixo-sampler-poly/Ramp.cpp \
-  /root/Bela/projects/agbaixo-sampler-poly/Ramp.h
-
-/root/Bela/projects/agbaixo-sampler-poly/Ramp.h:
